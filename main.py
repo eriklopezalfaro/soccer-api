6b9899b223a0d73467a4d18c90cb4f5256061f69
Erik Lopez <eriklopezalfaro@outlook.com>
2022-10-28 01:37:14 -0700

    Queries for specified player now instead of random leagues

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import requests
 import pprint as pp
+import requests
 
-url = "https://api-football-v1.p.rapidapi.com/v3/leagues"
+url = "https://api-football-v1.p.rapidapi.com/v3/players"
+
+querystring = {"league":"61","search":"neymar"}
 
 headers = {
 	"X-RapidAPI-Key": "f1654be039mshb33692f6178cb18p1977bbjsnbd55a7260427",
 	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers).json()
+response = requests.request("GET", url, headers=headers, params=querystring).json()
 
-# data = response['response'][0]
 for res in response['response']:
-    if response['response'].index(res) == 1:
-        break
-    data = res
-    pp.pprint(data, depth=4, indent=4)
+    pp.pprint(res, indent=3)
